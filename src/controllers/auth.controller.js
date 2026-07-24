ddcfb614c19d84e48122796c1cf20c8949619954
saanchijainnn <saanchi2315@gmail.com>
2026-07-24 21:26:22 +0530

    feat: implement registerUserController with validation for user input

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,3 +5,13 @@ const userModel = require("../models/user.model")
  * @description register a new user, expects username, email and password in the request body
  * @access Public
  */
+
+async function registerUserController(req, res) {
+
+    const { username, email, password } = req.body
+
+    if (!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide username, email and password"
+        })
+    }}
